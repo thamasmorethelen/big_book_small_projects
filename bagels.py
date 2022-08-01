b0b8cf3d903744dec00a251281603f56611b4395
Thomas Thelen <thamasthelen@gmail.com>
2022-08-01 15:45:45 -0700

    Create bagels.py

---
+++ bagels.py
@@ -0,0 +1,7 @@
+# User must guess three digit number
+# If they guess a number and it it in the wrong place return pico
+        # ex: number = 123. user guesses 132. print pico
+        # if user guesses 412 print pico pico
+        # if user guesses a number and it's in the right place print fermi
+        # if the user doesn't guess any correct numbers print bagels
+        

ca9ba91ece2168b60faeb3e3598b92b23eb18272
Thomas Thelen <thamasthelen@gmail.com>
2022-09-05 18:45:02 -0700

    Updates to dvd_bounce.py
    Declared constants, started main() function. Main currently creates logos and the programs main loop.

--- dvd_bounce.py
+++ dvd_bounce.py
@@ -1,15 +1,49 @@
-import bext
 import random
+from secrets import choice
+import sys
+import time
 
-
-width, height = bext.size()
 try:
-    while True:
-        bext.fg('random')
-        bext.bg('random')
-        x = random.randint(0, width - 1)
-        y = random.randint(0, height - 1)
-        bext.goto(x, y)
-        print('*', end='')
-except KeyboardInterrupt:
-    pass
+    import bext
+except ImportError:
+    print('This program requires the module BEXT')
+    print("To install bext follow the instructions in the link below")
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+# Costants
+WIDTH, HEIGHT = bext.size()
+WIDTH -= 1
+
+NUMBER_OF_LOGOS = 5 # See notes in book on page 26
+PAUSE_AMOUNT = 0.2 # See notes page 26
+COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+UP_RIGHT = 'ur'
+UP_LEFT = 'ul'
+DOWN_RIGHT = 'dr'
+DOWN_LEFT = 'dl'
+DIRECTIONS = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
+#Key names for logo dictionaries
+COLOR = 'color'
+X = 'x'
+Y = 'y'
+DIR = 'direction'
+
+
+def main():
+    bext.clear()
+
+    #generate some logos
+    logos = []
+    for i in range(NUMBER_OF_LOGOS):
+                        logos.append({COLOR: random.choice(COLORS),
+                        X: random.randint(1, WIDTH - 4),
+                        Y: random.randint(1, HEIGHT - 4),
+                        DIR: random.choice(DIRECTIONS)}
+        )
+        if logos[-1] % 2 == 0:
+            logos[-1] -+ 1
+    cornerBounces = 0 # counts how many times a logo hits a corner
+    while True: # main loop
+
+
